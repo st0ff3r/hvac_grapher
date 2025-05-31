695b393f3636ebf38c1d6ca3a8e6b775a2bf72ab
Kristoffer Ek <stoffer@skulp.net>
2025-06-01 00:44:32 +0200

    save to db working

--- save_in_db.pl
+++ save_in_db.pl
@@ -3,18 +3,22 @@
 use strict;
 use DBI;
 
-my $dbi = 'DBI:mysql:database=hvac;host=db;port=3306';
+my $mysql_database = $ENV{MYSQL_DATABASE} || die "no database supplied";
+my $mysql_user = $ENV{MYSQL_USER} || die "no database username supplied";
+my $mysql_password = $ENV{MYSQL_PASSWORD} || die "no database password supplied";
 
-my $dbh = DBI->connect($dbi, 'hvac', 'aibie7Kufae8TioC', { mysql_auto_reconnect => 1, mysql_enable_utf8 => 1 }) || die $!;
+my $dbi = 'DBI:mysql:database=' . $mysql_database . ';host=db;port=3306';
+
+my $dbh = DBI->connect($dbi, $mysql_user, $mysql_password, { mysql_auto_reconnect => 1, mysql_enable_utf8 => 1 }) || die $!;
 
 my $quoted_room_temp =			$ARGV[0] || die "need to supply all parameters";
 my $quoted_co2 =				$ARGV[1] || die "need to supply all parameters";
 my $quoted_supply_air_volume =	$ARGV[2] || die "need to supply all parameters";
 my $quoted_exhaust_air_volume =	$ARGV[3] || die "need to supply all parameters";
-my $quoted_outside_air =		$ARGV[4] || die "need to supply all parameters";
-my $quoted_outside_air_temp =	$ARGV[5] || die "need to supply all parameters";
-my $quoted_supply_air_temp =	$ARGV[6] || die "need to supply all parameters";
-my $quoted_return_air_temp =	$ARGV[7] || die "need to supply all parameters";
+my $quoted_outside_air_temp =	$ARGV[4] || die "need to supply all parameters";
+my $quoted_supply_air_temp =	$ARGV[5] || die "need to supply all parameters";
+my $quoted_return_air_temp =	$ARGV[6] || die "need to supply all parameters";
+my $quoted_exhaust_air_temp =	$ARGV[7] || die "need to supply all parameters";
 my $quoted_cooling =			$ARGV[8] || die "need to supply all parameters";
 my $quoted_heat_exchanger =		$ARGV[9] || die "need to supply all parameters";
 my $quoted_heating =			$ARGV[10] || die "need to supply all parameters";
@@ -24,10 +28,10 @@ $dbh->do(qq[INSERT INTO `samples` (
 				`co2`,
 				`supply_air_volume`,
 				`exhaust_air_volume`,
-				`outside_air`,
 				`outside_air_temp`,
 				`supply_air_temp`,
 				`return_air_temp`,
+				`exhaust_air_temp`,
 				`cooling`,
 				`heat_exchanger`,
 				`heating`,
@@ -37,10 +41,10 @@ $dbh->do(qq[INSERT INTO `samples` (
 				$quoted_co2,
 				$quoted_supply_air_volume,
 				$quoted_exhaust_air_volume,
-				$quoted_outside_air,
 				$quoted_outside_air_temp,
 				$quoted_supply_air_temp,
 				$quoted_return_air_temp,
+				$quoted_exhaust_air_temp,
 				$quoted_cooling,
 				$quoted_heat_exchanger,
 				$quoted_heating,
