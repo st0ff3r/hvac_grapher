9f47194470f36b1f5c91db7c3dae19a2fc62e21f
Kristoffer Ek <stoffer@skulp.net>
2025-06-01 01:49:14 +0200

    db setup procedure changed

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ make up
 
 To initialize db (only do it first time!):
 
-docker exec -it hvac-db /hvac_setup_db.sh
+docker cp hvac.sql hvac-db:/tmp/
+docker cp hvac_setup_db.sh hvac-db:/tmp/
+docker exec -it hvac-db /tmp/hvac_setup_db.sh
--- hvac_setup_db.sh
+++ hvac_setup_db.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD hvac < /hvac.sql &&
+mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD hvac < /tmp/hvac.sql
 
-#echo "GRANT SELECT,INSERT,UPDATE,DELETE ON root.* TO '*'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD &&
-#echo "GRANT SELECT,INSERT,UPDATE,DELETE ON nabovarme.* TO 'nabovarme'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD &&
-
-#echo "FLUSH PRIVILEGES" | mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD
-
-mysql_upgrade -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD
-
-#echo "import existing db by running:
-#	docker cp mysql_backup.sql.bz2 db:/tmp/	
-#	docker exec -it db /nabovarme_import.sh
-#	docker exec -it db /nabovarme_triggers.sh"
+if [ $? -eq 0 ]; then
+	echo "db has been set up"
+else
+	echo "db set up failed"
+fi
